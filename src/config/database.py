3c34958013ca47d64d1d5cc857702ea67992c430
AndrewHanasiro <andrewkhanasiro@gmail.com>
2023-06-03 11:49:56 -0300

    fix: CI use docker

--- src/config/database.py
+++ src/config/database.py
@@ -1,5 +1,4 @@
+import os
 from sqlalchemy import create_engine
 
-engine = create_engine(
-    "postgresql+psycopg2://root:db_password@localhost:5432/monetization", echo=True
-)
+engine = create_engine(os.environ["DATABASE_URL"], echo=True)
