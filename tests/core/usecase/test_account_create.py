2d980d68edc5d4102477d3cd87b84a12ca9c7d41
AndrewHanasiro <andrewkhanasiro@gmail.com>
2023-07-21 04:16:53 -0300

    more fix

--- tests/core/usecase/test_account_create.py
+++ tests/core/usecase/test_account_create.py
@@ -11,21 +11,21 @@ from src.core.usecase.driven.creating_account import CreatingAccount
 
 
 def test_should_create(session: Session):
-    id = uuid4()
+    id_ = uuid4()
     external_id = uuid4()
-    type = AccountType.PRE_PAID
+    type_ = AccountType.PRE_PAID
     # mock
     creating_account: CreatingAccount = AccountRepository(session)
     creating_account.create = MagicMock(
-        return_value=Account(id, external_id, type, True, datetime.now())
+        return_value=Account(id_, external_id, type_, True, datetime.now())
     )
     # usecase
     usecase = AccountCreate(creating_account)
-    result = usecase.create(external_id, type)
+    result = usecase.create(external_id, type_)
     # assert
-    assert result.id == id
+    assert result.id == id_
     assert result.external_id == external_id
-    assert result.type == type
+    assert result.type == type_
     assert result.is_enable
     assert isinstance(result.created_at, datetime)
-    creating_account.create.assert_called_once_with(external_id, type)
+    creating_account.create.assert_called_once_with(external_id, type_)
