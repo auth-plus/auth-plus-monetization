8f49b6f76ef814e2c90a731ce11d89b78e3a7ea8
AndrewSpreed <andrew@spreed.ai>
2025-12-10 04:50:05 -0300

    feat: update python and packages

--- src/presentation/kafka.py
+++ src/presentation/kafka.py
@@ -1,10 +1,13 @@
 import json
 
-from kafka import KafkaConsumer
+from kafka import (
+    KafkaConsumer,
+)
 
 from src.config.envvar import EnvVars
 from src.config.logger import console
 from src.core import Core
+from src.core.helpers import is_valid_uuid
 
 topics = [
     "2FA_EMAIL_CREATED",
@@ -15,24 +18,35 @@ topics = [
     "ORGANIZATION_CREATED",
 ]
 consumer = KafkaConsumer(
+    topics,
     value_deserializer=lambda m: json.loads(m.decode("ascii")),
     bootstrap_servers=[EnvVars.KAFKA_HOST],
+    auto_offset_reset="earliest",
+    enable_auto_commit=False,
 )
-consumer.subscribe(topics)
 
-
-for msg in consumer:
-    console.info(msg)
-    core = Core()
-    match msg.topic:
-        case [
-            "2FA_EMAIL_CREATED",
-            "2FA_PHONE_CREATED",
-            "2FA_EMAIL_SENT",
-            "2FA_PHONE_SENT",
-        ]:
-            account = core.receive_event.receive_event(msg.value["external_id"])
-        case "USER_CREATED":
-            account = core.account_create.create(msg.value["external_id"])
-        case "ORGANIZATION_CREATED":
-            account = core.account_create.create(msg.value["external_id"])
+try:
+    for msg in consumer:
+        console.info(msg)
+        core = Core()
+        try:
+            external_id = is_valid_uuid(msg.value["external_id"])
+        except Exception as e:
+            console.error(f"external_id UUID not valid: {e}")
+            continue
+        match msg.topic:
+            case [
+                "2FA_EMAIL_CREATED",
+                "2FA_PHONE_CREATED",
+                "2FA_EMAIL_SENT",
+                "2FA_PHONE_SENT",
+            ]:
+                core.receive_event.receive_event(external_id, msg.topic)
+            case "USER_CREATED":
+                core.account_create.create(external_id)
+            case "ORGANIZATION_CREATED":
+                core.account_create.create(external_id)
+except KeyboardInterrupt:
+    console.warning("Stopping consumer...")
+finally:
+    consumer.close()
